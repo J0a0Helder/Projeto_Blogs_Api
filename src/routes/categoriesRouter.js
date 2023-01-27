34a4ac93b8ee8a623fc0cba83018fb2ca506d4bf
João Hélder <joaoheldermartins@hotmail.com>
2023-01-27 20:21:07 -0300

    feat: adiciona listagem de todas as categorias através do metodo get no endpoint '/categories'

--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const categoriesController = require('../controllers/categoriesController');
+const tokenValidation = require('../middlewares/tokenMiddleware');
 
 const categoriesRouter = express.Router();
 
-categoriesRouter.get('/', categoriesController.getAll);
+categoriesRouter.get('/', tokenValidation, categoriesController.getAll);
 categoriesRouter.post('/', categoriesController.insertNew);
 
 module.exports = categoriesRouter;
