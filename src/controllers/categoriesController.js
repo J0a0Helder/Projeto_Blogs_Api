64c85a78aae022d6e85cd0dd2286ba9dc6257530
João Hélder <joaoheldermartins@hotmail.com>
2023-01-30 15:40:15 -0300

    feat: adiciona metodo de cadastro de novas categorias

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -5,12 +5,14 @@ const getAll = async (_req, res) => {
   res.status(200).json(categories);
 };
 
-// const insertNew = async (_req, res) => {
-//   const newProduct = await categoriesService.insertNew();
-//   res.status(201).send(newProduct);
-// };
+const insertNew = async (req, res) => {
+  const { name } = req.body;
+  const newCategory = await categoriesService.insertNew(name);
+  if (newCategory.type) return res.status(400).json({ message: newCategory.message });
+  res.status(201).send(newCategory);
+};
 
 module.exports = {
   getAll,
-  // insertNew,
+  insertNew,
 };
--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -6,6 +6,13 @@ const getAll = async () => {
   return categories;
 };
 
+const insertNew = async (name) => {
+  if (!name) return { type: 'MISSING_FIELDS', message: '"name" is required' };
+  const newCategory = await Category.create({ name });
+  return newCategory;
+};
+
 module.exports = {
   getAll,
+  insertNew,
 };
